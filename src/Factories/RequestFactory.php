6b71ddedc677bf13f4f2d689780a94bd4b6beb0a
Alin Eugen Deac <aedart@gmail.com>
2016-05-29 14:39:03 +0200

    Add request factory example
    Pretends to capture some kind of input, populates a request and also
    binds it to the IoC

---
+++ src/Factories/RequestFactory.php
@@ -0,0 +1,47 @@
+<?php namespace Aedart\React\Demo\Factories;
+
+use Aedart\React\Demo\Containers\IoC;
+use Aedart\React\Demo\Contracts\Requests\Request;
+use Aedart\React\Demo\Requests\InputRequest;
+
+/**
+ * Class RequestFactory
+ *
+ * Responsible for capturing requests and returns the
+ * concrete request implementation
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\React\Demo\Factories
+ */
+class RequestFactory
+{
+    static public function capture()
+    {
+        // For the sake of the example, we create a populated
+        // request object.
+        $request = new InputRequest([
+            'example.com',
+            'blog.wyrihaximus.net',
+            'wyrihaximus.net',
+            'github.com',
+            'google.com',
+            'facebook.com',
+        ]);
+
+        // TODO: Nice, if requests need to be obtained in many parts of the system!
+        // Obtain the ioc
+        $ioc = IoC::getInstance()->getContainer();
+
+        // Clear out any eventual previous request binding!
+        if(isset($ioc[Request::class])){
+            unset($ioc[Request::class]);
+        }
+
+        // We now must bind the current request to the IoC
+        $ioc->singleton(Request::class, $request);
+
+        // Finally return it
+        return $request;
+    }
+
+}
