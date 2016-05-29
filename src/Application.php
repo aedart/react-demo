258463f45761f898c3680409c14db751b3c35359
Alin Eugen Deac <aedart@gmail.com>
2016-05-29 11:49:26 +0200

    Add application and entry point
    Created a simple executable

---
+++ src/Application.php
@@ -0,0 +1,17 @@
+<?php namespace Aedart\React\Demo;
+
+/**
+ * Application
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\React\Demo
+ */
+class Application
+{
+
+    public function run()
+    {
+        print "Hallo world" . PHP_EOL;
+    }
+
+}
