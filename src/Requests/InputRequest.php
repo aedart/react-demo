2a0a0474960cc7100fa71d2e77914892da86f005
Alin Eugen Deac <aedart@gmail.com>
2016-05-29 14:37:47 +0200

    Add an input request
    Just as an example

---
+++ src/Requests/InputRequest.php
@@ -0,0 +1,14 @@
+<?php namespace Aedart\React\Demo\Requests;
+
+/**
+ * Input Request
+ *
+ * Example of an input-request, e.g. from the CLI
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\React\Demo\Requests
+ */
+class InputRequest extends RequestBase
+{
+
+}
